49a56cc721b38ceefd167ae28825f55a438e8acc
airkross <airkross123@gmail.com>
2023-04-25 02:38:02 +0300

    fixes layers

--- constants.ts
+++ constants.ts
@@ -1,1 +1,11 @@
-export const PORT = 3000
+import { DBConfig } from "./config/db";
+
+export const PORT = 3000;
+export const DB_CONFIG: DBConfig = {
+  host: "localhost",
+  port: 27017,
+  user: "admin",
+  pass: "123",
+  authSource: "todos-database",
+  authMechanism: "DEFAULT",
+};
--- server.ts
+++ server.ts
@@ -1,16 +1,8 @@
-import { PORT } from "./constants";
 import app from "./config/express";
-import { DBConfig, Database } from "./config/db";
+import { DB_CONFIG, PORT } from "./constants";
+import { Database } from "./config/db";
 
-const CONFIG: DBConfig = {
-  host: "localhost",
-  port: 27017,
-  user: "admin",
-  pass: "123",
-  authSource: "todos-database",
-  authMechanism: "DEFAULT",
-};
-const db = new Database(CONFIG);
+const db = new Database(DB_CONFIG);
 db.connect();
 
 app.listen(PORT, () => {
